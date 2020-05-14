691bf6a630720f4e058fd70a8a6ae844c83d7793
Lars Rosenquist <lrosenquist@pivotal.io>
2020-05-14 15:30:19 +0200

    Added force param

--- cf-update-config-server.sh
+++ cf-update-config-server.sh
@@ -3,7 +3,7 @@
 echo "Adding plugin repo..."
 cf add-plugin-repo CF-Community https://plugins.cloudfoundry.org
 echo "Installing SCS plugin..."
-cf install-plugin -r CF-Community "spring-cloud-services"
+cf install-plugin -r CF-Community "spring-cloud-services" -f
 echo "Logging in..."
 cf login -a $CF_URL -u $CF_USER -p $CF_PASSWORD
 echo "Adding secret to Credhub..."
