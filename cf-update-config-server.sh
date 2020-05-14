c55ef6fb6b718684522fe8ee8d156f59bdbcf9dd
Lars Rosenquist <lrosenquist@pivotal.io>
2020-05-14 15:15:57 +0200

    Fixed env vars

--- cf-update-config-server.sh
+++ cf-update-config-server.sh
@@ -5,7 +5,7 @@ cf add-plugin-repo CF-Community https://plugins.cloudfoundry.org
 echo "Installing SCS plugin..."
 cf install-plugin -r CF-Community "spring-cloud-services"
 echo "Logging in..."
-cf login -a $CF_URL -u ${CF_USER} -p ${CF_PASSWORD}
+cf login -a $CF_URL -u $CF_USER -p $CF_PASSWORD
 echo "Adding secret to Credhub..."
 cf config-server-add-credhub-secret my-config-server signing-demo/cloud/master/myprop '{"kid":"$KID","clientId":"$CLIENT_ID","clientSecret":"$CLIENT_SECRET"}'
 echo "Done..."
