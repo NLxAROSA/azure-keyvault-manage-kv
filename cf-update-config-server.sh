dcfa40cd7f9de55dc7827b4c4e2078cb47a9a17a
Lars Rosenquist <lrosenquist@pivotal.io>
2020-05-15 09:51:05 +0200

    env vars

--- cf-update-config-server.sh
+++ cf-update-config-server.sh
@@ -8,7 +8,7 @@ echo "Logging in..."
 cf login -a $CF_URL -u $CF_USER -p $CF_PASSWORD --skip-ssl-validation
 echo "Adding secret to Credhub..."
 config_json="cf config-server-add-credhub-secret my-config-server signing-demo/cloud/master/myprop '{"kid":"$KID","clientId":"$CLIENT_ID","clientSecret":"$CLIENT_SECRET"}'"
-echo config_json
-echo "cf config-server-add-credhub-secret my-config-server signing-demo/cloud/master/myprop '{"kid":"$KID","clientId":"$CLIENT_ID","clientSecret":"$CLIENT_SECRET"}'"
+echo $config_json
+echo "cf config-server-add-credhub-secret my-config-server signing-demo/cloud/master/myprop $config_json"
 cf config-server-add-credhub-secret my-config-server signing-demo/cloud/master/myprop $config_json
 echo "Done..."
