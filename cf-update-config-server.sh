f3d268d9d229bc18bfd2ccf33b741c623f740870
Lars Rosenquist <lrosenquist@pivotal.io>
2020-05-15 09:57:35 +0200

    env vars

--- cf-update-config-server.sh
+++ cf-update-config-server.sh
@@ -7,7 +7,7 @@ cf install-plugin -r CF-Community "spring-cloud-services" -f
 echo "Logging in..."
 cf login -a $CF_URL -u $CF_USER -p $CF_PASSWORD --skip-ssl-validation
 echo "Adding secret to Credhub..."
-config_json='{"kid":"$KID","clientId":"$CLIENT_ID","clientSecret":"$CLIENT_SECRET"}'
+config_json='{"kid":"'$KID'","clientId":"'$CLIENT_ID'","clientSecret":"'$CLIENT_SECRET'"}'
 echo $config_json
 echo "cf config-server-add-credhub-secret my-config-server signing-demo/cloud/master/myprop $config_json"
 cf config-server-add-credhub-secret my-config-server signing-demo/cloud/master/myprop $config_json
